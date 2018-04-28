7940f7b5c3b4a066a0c7cdfb1808e280fd53b17b
JohnTheBoss <kollar.janos@knightnet.hu>
2018-04-28 10:05:27 +0200

    ticket 404

--- src/app/ticket/bid/bid.component.ts
+++ src/app/ticket/bid/bid.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {TicketService} from '../../shared/ticket.service';
 import {TicketModel} from '../../shared/ticket-model';
 import {UserService} from '../../shared/user.service';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 
 @Component({
   selector: 'app-bid',
@@ -12,14 +13,29 @@ export class BidComponent implements OnInit {
   ticket: TicketModel;
   isLoggedIn: boolean;
 
-  constructor(private _ticketService: TicketService, _userService: UserService) {
+  constructor(private _ticketService: TicketService, _userService: UserService, private _route: ActivatedRoute, private _router: Router) {
     this.isLoggedIn = _userService.isLoggedin;
   }
 
   ngOnInit() {
-    const id = '-Ky0HolLJBH3Q5uVHWZf';
-    this._ticketService.getOne(id).subscribe(
-      ticket => this.ticket = ticket
+    const handle404 = () => {
+      this._router.navigate(['404']);
+    };
+    this._route.paramMap.subscribe(
+      (params: ParamMap) => {
+        const id = params.get('id') || '';
+        this._ticketService.getOne(id).subscribe(
+          ticket => {
+            if (ticket === null) {
+              handle404();
+            } else {
+              this.ticket = ticket;
+            }
+          }, () => {
+            return handle404();
+          }
+        );
+      }
     );
   }
 
