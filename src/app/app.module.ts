de4b37331a4463542741635a54123357eb8c5181
Kollár János <kollar.janos@knightnet.hu>
2018-09-22 17:28:20 +0200

    init firebase sdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,29 @@
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { AlertModule, CollapseModule } from 'ngx-bootstrap';
-import { AppRoutingModule } from './app-routing.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {AlertModule, CollapseModule} from 'ngx-bootstrap';
+import {AppRoutingModule} from './app-routing.module';
 
-import { AppComponent } from './app.component';
-import { FooterComponent } from './core/footer/footer.component';
-import { JumbotronComponent } from './core/jumbotron/jumbotron.component';
-import { NavbarComponent } from './core/navbar/navbar.component';
-import { EventcardComponent } from './event/eventcard/eventcard.component';
-import { AuthInterceptor } from './shared/auth-interceptor';
-import { EventService } from './shared/event.service';
-import { LoggedInGuardGuard } from './shared/logged-in-guard.guard';
-import { TicketService } from './shared/ticket.service';
-import { UserService } from './shared/user.service';
-import { TicketDetailsCardComponent } from './ticket/ticket-details-card/ticket-details-card.component';
-import { BiddingCardComponent } from './ticket/bidding-card/bidding-card.component';
+import {AppComponent} from './app.component';
+import {FooterComponent} from './core/footer/footer.component';
+import {JumbotronComponent} from './core/jumbotron/jumbotron.component';
+import {NavbarComponent} from './core/navbar/navbar.component';
+import {EventcardComponent} from './event/eventcard/eventcard.component';
+import {AuthInterceptor} from './shared/auth-interceptor';
+import {EventService} from './shared/event.service';
+import {LoggedInGuardGuard} from './shared/logged-in-guard.guard';
+import {TicketService} from './shared/ticket.service';
+import {UserService} from './shared/user.service';
+import {TicketDetailsCardComponent} from './ticket/ticket-details-card/ticket-details-card.component';
+import {BiddingCardComponent} from './ticket/bidding-card/bidding-card.component';
 import {MomentModule} from 'angular2-moment';
 import 'moment/locale/hu';
-import { BidFormComponent } from './ticket/bid-form/bid-form.component';
-import { LoadingSpinnerComponent } from './core/loading-spinner/loading-spinner.component';
+import {BidFormComponent} from './ticket/bid-form/bid-form.component';
+import {LoadingSpinnerComponent} from './core/loading-spinner/loading-spinner.component';
 import {BidService} from './shared/bid.service';
+import * as firebase from 'firebase';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -61,4 +63,7 @@ import {BidService} from './shared/bid.service';
   bootstrap: [AppComponent]
 })
 export class AppModule {
+  constructor() {
+    firebase.initializeApp(environment.firebase);
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,11 @@ export const environment = {
     baseUrl: 'https://jegybazar-31517.firebaseio.com',
     registrationUrl: ' https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser',
     loginUrl: ' https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword',
-    apikey: 'AIzaSyDbBQZQI_o2YRNGoKCzs3G7RrGl0Amsg74'
+    apiKey: 'AIzaSyDbBQZQI_o2YRNGoKCzs3G7RrGl0Amsg74',
+    authDomain: 'jegybazar-31517.firebaseapp.com',
+    databaseURL: 'https://jegybazar-31517.firebaseio.com',
+    projectId: 'jegybazar-31517',
+    storageBucket: 'jegybazar-31517.appspot.com',
+    messagingSenderId: '944032524552'
   }
 };
